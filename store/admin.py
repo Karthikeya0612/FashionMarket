64839c4a694f2e088e308c35f26bc2c1ef234b0a
Karthikeya0612 <uppukarthikeya2002@gmail.com>
2023-10-21 13:25:41 +0530

    modifying views and categories page

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from store.models import Product, Destination, ProductsOffered
+from store.models import Destination, ProductsOffered, Carousel
 
 # Register your models here.
-admin.site.register(Product)
 admin.site.register(Destination)
 admin.site.register(ProductsOffered)
+admin.site.register(Carousel)
--- store/views.py
+++ store/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect
 from . import models
 
 results=models.ProductsOffered.objects
-categories_list = models.ProductsOffered.objects.values_list('category', flat=True).distinct()
+categories_list = results.values_list('category', flat=True).distinct()
+carousels = models.Carousel.objects
 
 def home(request):
     dests = models.Destination.objects.all()
-    return render(request, 'store/index.html', {'dests' : dests})
+    carousels_data = carousels.filter(label="home")
+    return render(request, 'store/index.html', {'dests' : dests, 'carousels_data': carousels_data})
 
 
 def show_category(request, category):
@@ -17,8 +19,8 @@ def show_category(request, category):
             label = product.label
         else:
             label = None
-
-        context = {'category_data': data, 'category_name': category.capitalize(), 'label_name': label.capitalize()}
+        carousels_data = list(carousels.filter(label=label))
+        context = {'category_data': data, 'category_name': category.capitalize(), 'label_name': label.capitalize(), 'carousels_data': carousels_data}
 
     else:
         return redirect("..")
